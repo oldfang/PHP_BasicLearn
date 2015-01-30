c4f6f6bd1aa623aad3b1d905eb09246627188b4e
oldfang <fang_wolf@yeah.net>
2015-01-30 11:13:42 +0800

    创建项目
    创建项目

---
+++ template/index.php
@@ -0,0 +1,33 @@
+<?php
+	ini_set("display_errors",1);
+	error_reporting(E_ALL | E_DISTRICT);
+	
+	include("header.html");
+?>
+
+<h2>Welcome to a J.D. Salinger Fan Club</h2> 
+<p>
+	Lorem ipsum dolor sit amet, 
+	consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+	dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+	laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+	reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+	deserunt mollit anim id est laborum.
+</p> 
+
+<h2>Another Header</h2> 
+
+<p>
+	Lorem ipsum dolor sit amet, 
+	consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et 
+	dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+	laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+	reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+	deserunt mollit anim id est laborum.
+</p> 
+	
+<?php	
+	require("footer.html");
+?>
